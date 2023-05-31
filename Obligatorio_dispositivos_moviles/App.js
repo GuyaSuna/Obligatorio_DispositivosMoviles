a227d99031b00360756892a2f9b19e8a3914c582
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-05-31 11:31:47 -0300

    Prueba Funcionamiento

--- Obligatorio_dispositivos_moviles/App.js
+++ Obligatorio_dispositivos_moviles/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MensajitoNahuel from './Componentes/MensajitoNahuel';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <MensajitoNahuel/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Obligatorio_dispositivos_moviles/Componentes/MensajitoNahuel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View , Text, Button } from 'react-native'
+
+const MensajitoNahuel = () => {
+  return (
+    <View>
+      <Text> Hola pibres, estoy haciendo pruebitas</Text>
+    </View>
+  )
+}
+
+export default MensajitoNahuel
