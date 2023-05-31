018d9cf1f2d9497fa4cd9ff6a29934301b56a82c
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-05-31 12:13:16 -0300

    Prueba de Componentes

--- Obligatorio_dispositivos_moviles/App.js
+++ Obligatorio_dispositivos_moviles/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MensajitoNahuel from './Componentes/MensajitoNahuel';
+import Contador from './Componentes/Contador';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Contador/>
       <MensajitoNahuel/>
       <StatusBar style="auto" />
     </View>
