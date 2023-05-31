1ab7d91c0bc8d0eb883de289ad319483e14ec14c
AlexSumner <111889133+AlexSumner@users.noreply.github.com>
2023-05-31 11:36:20 -0300

    Create Mensajito Alex.js

---
+++ Obligatorio_dispositivos_moviles/Componentes/Mensajito Alex.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+const MensajitoAlex = () => {
+  return (
+    <View>
+      <Text> Hola pibres, estoy haciendo pruebitas</Text>
+    </View>
+  );
+};
+
+export default MensajitoAlex;
