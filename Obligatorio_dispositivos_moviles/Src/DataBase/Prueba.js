7d596055b2972d03123c43c2674a01c27f3d6206
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-06-01 13:22:11 -0300

    PruebitaPruebasa

---
+++ Obligatorio_dispositivos_moviles/Src/DataBase/Prueba.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text }from 'react-native'
+const Prueba = () => {
+  return (
+    <View>
+      <Text>
+            Holasa
+      </Text>
+    </View>
+  )
+}
+
+export default Prueba
