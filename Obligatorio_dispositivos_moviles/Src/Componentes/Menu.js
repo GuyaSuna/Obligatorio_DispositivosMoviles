e39ce1ead8ded488ed5026cd1ac38c68ba8ddd8a
AlexSumner <111889133+AlexSumner@users.noreply.github.com>
2023-06-01 13:17:35 -0300

    Update Menu.js

--- Obligatorio_dispositivos_moviles/Src/Componentes/Menu.js
+++ Obligatorio_dispositivos_moviles/Src/Componentes/Menu.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Menu =()=> {
-    return (
-        <view>Esto es un componente!!!!</view>
-    )
-}
+const Menu = () => {
+  return (
+    <View style={styles.overlay}>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>USUARIOS</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>ZONAS</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>INSUMOS</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>OBSERVACIONES</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>TRATAMIENTOS</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Componente 6</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default Menu
+export default Menu;
