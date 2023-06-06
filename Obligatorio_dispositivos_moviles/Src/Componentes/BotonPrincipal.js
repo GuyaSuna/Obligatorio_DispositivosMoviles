0c8708fe555e8679619a38a1d09c003bb9d320b5
SoyYoPapa <email@example.com>
2023-06-06 19:43:28 -0300

    Nuevo Boton
    Boton reutilizable para la pagina main

---
+++ Obligatorio_dispositivos_moviles/Src/Componentes/BotonPrincipal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    ImageBackground,
+  } from "react-native";
+const BotonPrincipal = () => {
+  return (
+    <View>BotonPrincipal</View>
+  )
+}
+
+export default BotonPrincipal
