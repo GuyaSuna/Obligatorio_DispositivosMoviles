e6f334f41dae3c56044d78ad95c2b2ff445bdf5d
AleSpinelli2 <alejospinelli2@gmail.com>
2023-06-07 11:01:11 -0300

    Merge branch 'Alex' into Alejo

---
+++ Obligatorio_dispositivos_moviles/Src/Componentes/MyInputText.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { StyleSheet, TextInput, View} from "react-native";
+
+const MyInputText = ({
+    maxLength : 40,
+    minLength : 0,
+    onChangeText : () => console.log("change text"),
+    placeholder : "placeholder",
+    keyboardType : "default",
+    secureTextEntry : false,
+    returnKeyType : "done",
+    numberOfLines : 1,
+    mutiline : false,
+    onSubmitEditing : () => console.log("submit editing"),
+    blurOnSubmit : false,
+    value : "",
+    defaultValue : "",
+}) => {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          maxLength={maxLength}
+          minLength={minLength}
+          onChangeText={onChangeText}
+          placeholder={placeholder}
+          placeholderTextColor="gray"
+          keyboardType={keyboardType}
+          secureTextEntry={secureTextEntry}
+          returnKeyType={returnKeyType}
+          numberOfLines={numberOfLines}
+          mutiline={mutiline}
+          onSubmitEditing={onSubmitEditing}
+          blurOnSubmit={blurOnSubmit}
+          value={value}
+          defaultValue={defaultValue}
+        />
+      </View>
+    );
+  };
+  
+  export default MyInputText;
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginLeft: 30,
+      marginRight: 30,
+      marginTop: 10,
+      marginBottom: 10,
+      borderColor: '#d3d3d3',
+      borderWidth: 1,
+      padding: 10
+    },
+    input: {
+      color: 'black',
+    },
+  });
+  
