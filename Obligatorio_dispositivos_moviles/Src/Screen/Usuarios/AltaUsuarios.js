52361a8bb361f06dfbac788bb7934c8c95101470
AlexSumner <111889133+AlexSumner@users.noreply.github.com>
2023-06-14 09:50:49 -0300

    Update AltaUsuarios.js

--- Obligatorio_dispositivos_moviles/Src/Screen/Usuarios/AltaUsuarios.js
+++ Obligatorio_dispositivos_moviles/Src/Screen/Usuarios/AltaUsuarios.js
@@ -1,133 +1,143 @@
-import React, {useState} from "react";
-import {StyleSheet, Text, View, SafeAreaView, ScrollView, KeyboardAvoidingView,Alert} from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  KeyboardAvoidingView,
+  Alert,
+} from "react-native";
 import MyInputText from "../../Componentes/MyInputText";
 import BotonPrincipal from "../../Componentes/BotonPrincipal";
-import {useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import DatabaseConnection from "../../DataBase/dbConnection";
 
 const db = DatabaseConnection.getConnection();
 
 const AltaUsuario = () => {
+  //Aca definimos los estados de los campos del form
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-//Aca definimos los estados de los campos del form
-const [userName, setUserName] = useState("");
-const [password, setPassword] = useState("");
-const [email, setEmail] = useState("");
+  const navigation = useNavigation();
 
-const navigation = useNavigation();
-
-//Este es el metodo que setea los estados
-const handleUserName= (userName) =>{
+  //Este es el metodo que setea los estados
+  const handleUserName = (userName) => {
     setUserName(userName);
-}
-const handlePassword = (password) =>{
+  };
+  const handlePassword = (password) => {
     setPassword(password);
-}
-const handleEmail = (email) =>{
+  };
+  const handleEmail = (email) => {
     setEmail(email);
-}
+  };
 
-const addUser = ()=>{
+  const addUser = () => {
     console.log("Agregar Usuario");
 
-    if(validateDate()){
-        console.log("Usuario gurdado");
-        db.transaction((tx)=>{
-            tx.executeSql(
-                'INSERT INTO users (userName, password, email) VALUES(?,?,?)',
-                [userName, password, email],
-                (tx, results) => {
-                    if(results.rowAffected > 0){
-                        Alert.alert("Exito" , "usuario registrado con exito",[
-                            {
-                                text: "Ok",
-                                onPress:()=> navigation.navigate("HomeScreen"),
-                            }
-                        ],
-                        {
-                            cancelable: false
-                        } );
-                        clearData();
-                    }else{
-                        Alert.alert("Error","Error al registrar usuario");
-                    }           
+    if (validateDate()) {
+      console.log("Usuario gurdado");
+      db.transaction((tx) => {
+        tx.executeSql(
+          "INSERT INTO users (userName, password, email) VALUES(?,?,?)",
+          [userName, password, email],
+          (tx, results) => {
+            if (results.rowAffected > 0) {
+              Alert.alert(
+                "Exito",
+                "usuario registrado con exito",
+                [
+                  {
+                    text: "Ok",
+                    onPress: () => navigation.navigate("PaginaPrincipa;"),
+                  },
+                ],
+                {
+                  cancelable: false,
                 }
-            )
-        } ); 
+              );
+              clearData();
+            } else {
+              Alert.alert("Error", "Error al registrar usuario");
+            }
+          }
+        );
+      });
     }
-}
+  };
 
-const validateDate =()=>{
-    if(userName === "" && !userName.trim()){
-        Alert.alert("Error","El nombre de usuario es obligatorio");
-        return false;
+  const validateDate = () => {
+    if (userName === "" && !userName.trim()) {
+      Alert.alert("Error", "El nombre de usuario es obligatorio");
+      return false;
     }
-    if(password ==="" && !password.trim()){
-        Alert.alert("Error", "La contraseña es obligatoria");
-        return false;
+    if (password === "" && !password.trim()) {
+      Alert.alert("Error", "La contraseña es obligatoria");
+      return false;
     }
-    if(!email.trim()){
-        Alert.alert("Error","El correo es ovbligatorio");
-        return false;
+    if (!email.trim()) {
+      Alert.alert("Error", "El correo es ovbligatorio");
+      return false;
     }
-    if(!email.trim("@")){
-        Alert.alert("Error", "El correo no es valido")
-        return false;
+    if (!email.trim("@")) {
+      Alert.alert("Error", "El correo no es valido");
+      return false;
     }
 
     return true;
-}
-const clearData = () =>{
+  };
+  const clearData = () => {
     setUserName("");
     setPassword("");
     setEmail("");
-}
+  };
 
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View>
+          <ScrollView>
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
+              <MyInputText
+                styles={styles.inputUser}
+                placeholder="Nombre de usuario"
+                value={userName}
+                onChangeText={handleUserName}
+              />
+              <MyInputText
+                styles={styles.inputPassword}
+                placeholder="Contraseña"
+                value={password}
+                onChangeText={handlePassword}
+                minLength={8}
+                maxLength={16}
+              />
+              <MyInputText
+                styles={styles.inputEmail}
+                placeholder="Correo"
+                value={email}
+                onChangeText={handleEmail}
+                keyboardType="email-address"
+              />
+              <BotonPrincipal
+                title="Registrar Usuario"
+                btnColor="green"
+                onPress={addUser}
+              />
+            </KeyboardAvoidingView>
+          </ScrollView>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-    return ( 
-        <SafeAreaView style={styles.container}>
-            <View>
-                <View>
-                    <ScrollView>
-                        <KeyboardAvoidingView behavior="padding" style={styles.container}>
-                            <MyInputText
-                            styles={styles.inputUser}
-                            placeholder="Nombre de usuario"
-                            value={userName}
-                            onChangeText={handleUserName}
-                            />
-                            <MyInputText 
-                            styles={styles.inputPassword}
-                            placeholder="Contraseña"
-                            value={password}
-                            onChangeText={handlePassword}
-                            minLength={8}
-                            maxLength={16}
-                            />
-                            <MyInputText
-                            styles={styles.inputEmail}
-                            placeholder="Correo"
-                            value={email}
-                            onChangeText={handleEmail}
-                            keyboardType="email-address"
-                            />
-                            <BotonPrincipal
-                            title="Registrar Usuario"
-                            btnColor="green"
-                            onPress={addUser}
-                            />
-                        </KeyboardAvoidingView>
-                    </ScrollView>
-                </View>
-            </View>
-        </SafeAreaView>
-     );
-}
- 
 export default AltaUsuario;
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
   button: {
     flex: 1,
     alignContent: "center",
