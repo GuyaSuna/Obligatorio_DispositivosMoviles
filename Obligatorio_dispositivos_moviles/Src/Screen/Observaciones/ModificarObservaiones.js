9fa237882f27f19cfb48650ee7bc01c0b4567694
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-07-07 12:37:40 -0300

    correccion para android

--- Obligatorio_dispositivos_moviles/Src/Screen/Observaciones/ModificarObservaiones.js
+++ Obligatorio_dispositivos_moviles/Src/Screen/Observaciones/ModificarObservaiones.js
@@ -217,16 +217,12 @@ const styles = StyleSheet.create({
   picker: {
     height: 200,
     marginBottom: 5,
-    borderRadius: '5px',
-    borderWidth: '2px',
     borderColor: 'grey',
   },
   map: {
     width: '100%',
     height: 200,
     marginBottom: 10,
-    borderRadius: '5px',
-    borderWidth: '2px',
     borderColor: 'grey',
   },
   image: {
@@ -237,9 +233,7 @@ const styles = StyleSheet.create({
   locationText: {
     fontSize: 16,
     fontWeight: 'bold',
-    borderWidth: '2px',
     borderColor: 'grey',
-    borderRadius: '5px',
   },
   buttonContainer: {
     marginBottom: 10,
@@ -247,7 +241,6 @@ const styles = StyleSheet.create({
   listItemView: {
     backgroundColor: "white",
     padding: 10,
-    borderRadius: 10,
     margin:10,
   },
 });
